1f0421c470c06636fe10893ef7aafba7306fa3e8
jeremyhaelewyn <jeremha18@gmail.com>
2018-05-05 23:37:17 +0200

    Ajout classe Environment

---
+++ Project/Traffic_Simulation/src/main/generated-sources/sarl/environments/Environment.java
@@ -0,0 +1,18 @@
+package environments;
+
+import io.sarl.lang.annotation.SarlElementType;
+import io.sarl.lang.annotation.SarlSpecification;
+import io.sarl.lang.annotation.SyntheticMember;
+
+/**
+ * @author jerem
+ */
+@SarlSpecification("0.7")
+@SarlElementType(10)
+@SuppressWarnings("all")
+public class Environment {
+  @SyntheticMember
+  public Environment() {
+    super();
+  }
+}
