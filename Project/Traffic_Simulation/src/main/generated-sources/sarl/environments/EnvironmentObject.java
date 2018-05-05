6d0a751b157efae887a0374a9f283169c205721d
jeremyhaelewyn <jeremha18@gmail.com>
2018-05-06 00:12:58 +0200

    Ajout classe Point

--- Project/Traffic_Simulation/src/main/generated-sources/sarl/environments/EnvironmentObject.java
+++ Project/Traffic_Simulation/src/main/generated-sources/sarl/environments/EnvironmentObject.java
@@ -1,8 +1,11 @@
 package environments;
 
+import environments.Point;
 import io.sarl.lang.annotation.SarlElementType;
 import io.sarl.lang.annotation.SarlSpecification;
 import io.sarl.lang.annotation.SyntheticMember;
+import java.util.ArrayList;
+import org.eclipse.xtext.xbase.lib.Pure;
 
 /**
  * @author jerem
@@ -11,8 +14,25 @@ import io.sarl.lang.annotation.SyntheticMember;
 @SarlElementType(10)
 @SuppressWarnings("all")
 public abstract class EnvironmentObject {
-  @SyntheticMember
+  private final ArrayList<Point> points;
+  
   public EnvironmentObject() {
-    super();
+    ArrayList<Point> _arrayList = new ArrayList<Point>();
+    this.points = _arrayList;
+  }
+  
+  @Override
+  @Pure
+  @SyntheticMember
+  public boolean equals(final Object obj) {
+    return super.equals(obj);
+  }
+  
+  @Override
+  @Pure
+  @SyntheticMember
+  public int hashCode() {
+    int result = super.hashCode();
+    return result;
   }
 }
