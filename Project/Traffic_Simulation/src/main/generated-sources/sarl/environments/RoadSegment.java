5a3f35a19408b9102e1c80f9f6216e720b88b714
jeremyhaelewyn <jeremha18@gmail.com>
2018-05-06 00:03:54 +0200

    Ajout classe EnvironmentObject

--- Project/Traffic_Simulation/src/main/generated-sources/sarl/environments/RoadSegment.java
+++ Project/Traffic_Simulation/src/main/generated-sources/sarl/environments/RoadSegment.java
@@ -1,9 +1,11 @@
 package environments;
 
+import environments.EnvironmentObject;
 import environments.RoadConnection;
 import io.sarl.lang.annotation.SarlElementType;
 import io.sarl.lang.annotation.SarlSpecification;
 import io.sarl.lang.annotation.SyntheticMember;
+import java.util.ArrayList;
 import org.eclipse.xtext.xbase.lib.Pure;
 
 /**
@@ -17,11 +19,15 @@ public class RoadSegment {
   
   private final RoadConnection end;
   
+  private final ArrayList<EnvironmentObject> objects;
+  
   public RoadSegment() {
     RoadConnection _roadConnection = new RoadConnection();
     this.start = _roadConnection;
     RoadConnection _roadConnection_1 = new RoadConnection();
     this.end = _roadConnection_1;
+    ArrayList<EnvironmentObject> _arrayList = new ArrayList<EnvironmentObject>();
+    this.objects = _arrayList;
   }
   
   @Override
