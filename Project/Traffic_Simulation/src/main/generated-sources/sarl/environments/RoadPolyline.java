493d5e9c86c4e8affd4d22b46a934db40da9abe6
jeremyhaelewyn <jeremha18@gmail.com>
2018-05-06 00:00:49 +0200

    Ajout classe RoadPolyline

---
+++ Project/Traffic_Simulation/src/main/generated-sources/sarl/environments/RoadPolyline.java
@@ -0,0 +1,19 @@
+package environments;
+
+import environments.RoadSegment;
+import io.sarl.lang.annotation.SarlElementType;
+import io.sarl.lang.annotation.SarlSpecification;
+import io.sarl.lang.annotation.SyntheticMember;
+
+/**
+ * @author jerem
+ */
+@SarlSpecification("0.7")
+@SarlElementType(10)
+@SuppressWarnings("all")
+public class RoadPolyline extends RoadSegment {
+  @SyntheticMember
+  public RoadPolyline() {
+    super();
+  }
+}
